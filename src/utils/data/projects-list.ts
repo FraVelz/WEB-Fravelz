bb0fa7084a9d367cd585ceb3c0a570e45b166916
Fravelz <fravelz_@hotmail.com>
2026-04-27 07:11:35 -0500

    feat: update url image for HeroSection feat: add new image for project frontend-mentor-nextjs

--- src/utils/data/projects-list.ts
+++ src/utils/data/projects-list.ts
@@ -2,8 +2,15 @@ import type { Project } from "./project-types";
 
 import { projectNotasHacking } from "./projects/project-notas-hacking";
 import { projectIcfesMaster } from "./projects/project-icfes-master";
+import { projectFrontendMentorNextjs } from "./projects/project-frontend-mentor-nextjs";
 import { projectWebTodoList } from "./projects/project-web-todo-list";
 import { projectWebMarcadores } from "./projects/project-web-marcadores";
 
 /** Lista de todos los proyectos del portafolio */
-export const projects: Project[] = [projectIcfesMaster, projectWebTodoList, projectWebMarcadores, projectNotasHacking];
+export const projects: Project[] = [
+  projectIcfesMaster,
+  projectFrontendMentorNextjs,
+  projectWebTodoList,
+  projectWebMarcadores,
+  projectNotasHacking,
+];
