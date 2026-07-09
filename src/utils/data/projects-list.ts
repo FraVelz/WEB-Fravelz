9443da9da0ce8e0caac6cdffdd2d1f67b62a8b8d
Fravelz <fravelz_@hotmail.com>
2026-07-09 10:43:19 -0500

    refactor(projects): reorder project list for home highlights

--- src/utils/data/projects-list.ts
+++ src/utils/data/projects-list.ts
@@ -1,27 +1,27 @@
 import type { Project } from "./project-types";
 
-import { projectFravelzPortfolio } from "./projects/project-fravelz-portfolio";
-import { projectNotasHacking } from "./projects/project-notas-hacking";
 import { projectIcfesMaster } from "./projects/project-icfes-master";
-import { projectFrontendMentorNextjs } from "./projects/project-frontend-mentor-nextjs";
-import { projectWebTodoList } from "./projects/project-web-todo-list";
+import { projectEcoBogota } from "./projects/project-eco-bogota";
+import { projectStarcrypt } from "./projects/project-starcrypt";
 import { projectWebMarcadores } from "./projects/project-web-marcadores";
 import { projectWebProsperity } from "./projects/project-web-prosperity";
+import { projectWebTodoList } from "./projects/project-web-todo-list";
 import { projectAyudandoAndo } from "./projects/project-ayudando-ando";
 import { projectFvStore } from "./projects/project-fv-store";
-import { projectStarcrypt } from "./projects/project-starcrypt";
-import { projectEcoBogota } from "./projects/project-eco-bogota";
+import { projectFrontendMentorNextjs } from "./projects/project-frontend-mentor-nextjs";
+import { projectFravelzPortfolio } from "./projects/project-fravelz-portfolio";
+import { projectNotasHacking } from "./projects/project-notas-hacking";
 
 /** Lista de todos los proyectos del portafolio */
 export const projects: Project[] = [
   projectIcfesMaster,
+  projectEcoBogota,
+  projectStarcrypt,
   projectWebMarcadores,
   projectWebProsperity,
+  projectWebTodoList,
   projectAyudandoAndo,
   projectFvStore,
-  projectStarcrypt,
-  projectEcoBogota,
-  projectWebTodoList,
   projectFrontendMentorNextjs,
   projectFravelzPortfolio,
   projectNotasHacking,
